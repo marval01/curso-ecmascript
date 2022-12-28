c86a30936b28a090554b10cfbe80b1340dc6f364
martincv <sonmartin94@gmail.com>
2022-12-28 12:55:03 -0500

    strings ecmascript

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,11 @@
+function square(num){
+    return num * num;
+}
+
+const square = (num)=>{
+    return num *  num;
+}
+
+//Un solo argumento, permite omitir los parentesis y los cochetes.
+const square = num => num * num;
+
