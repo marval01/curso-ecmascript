52f2f186899b9d3c06be60e6db0aa23057c518da
martincv <sonmartin94@gmail.com>
2022-12-29 13:41:19 -0500

    Trabajando con modulos

--- src/es6/07-clases.js
+++ src/es6/07-clases.js
@@ -63,4 +63,15 @@ class user {
     greeting(){
         return `${this.speak()} ${this.name}`;
     }
+
+    get uAge(){
+        return this.age;
+    }
+    set uAge(n){
+        this.age = n;
+    }
 }
+
+const bebeloper1 = new user('David', 15);
+console.log(bebeloper1.uAge);
+console.log(bebeloper1.uAge = 20);
---
+++ src/es6/08-module.js
@@ -0,0 +1,8 @@
+/*
+Si trabajamos con js vanilla, debemos especificar en 
+el package.json que trabajaremos con modulos: "type":"module"
+*/
+
+import hello from "./module.js";
+
+hello();
