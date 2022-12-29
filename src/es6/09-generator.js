24935ff835f4bd19f10925e660dd93ed5b4af97e
martincv <sonmartin94@gmail.com>
2022-12-29 14:04:45 -0500

    Set y add en javascript

--- src/es6/09-generator.js
+++ src/es6/09-generator.js
@@ -1,2 +1,15 @@
 //sintaxis del generator, se usa una asterisco despues de declarar la funcion.
-function*
+function* iterate(array){
+    for (let value of array){
+        //palabra reservada que retorna cada valor segun sea el caso.
+        yield value;
+    }
+}
+
+const it = iterate(["Martín", "Davis","Ana", "Ulises","jennifer"]);
+console.log(it.next().value);//.next() metodo que se crea dentro de nuestro generator
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
---
+++ src/es6/10-set-add.js
@@ -0,0 +1,7 @@
+//Set es una nueva estructura de datos para almacenar elementos únicos, es decir, sin elementos repetidos.
+const list = new Set()
+
+list.add("item 1");
+list.add("item 2").add("item 3");
+
+console.log(list);
